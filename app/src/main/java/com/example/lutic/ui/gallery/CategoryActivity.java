b8a97defb95b8faa741a72e813b9faab886a0e65
Mezeo-22 <ktoto.tam2005@mail.ru>
2022-12-23 08:08:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lutic/ui/gallery/CategoryActivity.java
@@ -0,0 +1,17 @@
+package com.example.lutic.ui.gallery;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lutic.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_gallery);
+    }
+}
